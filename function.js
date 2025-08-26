440a89a9d091ba374085e17d9b5ef00cf2a640a4
RomanUrquijo <reurquijol@ufpso.edu.co>
2025-08-26 10:20:07 -0500

    modificando scrit

---
+++ function.js
@@ -0,0 +1,12 @@
+ function CambiarColor(){
+            //alert("HOLA BIENVENIDO");
+            document.body.style.backgroundColor = "black";
+            document.getElementById("title").style.color= "orange";
+        }
+
+
+         function CambiarColore(){
+            //alert("HOLA BIENVENIDO");
+            document.body.style.backgroundColor = "green";
+            document.getElementById("title").style.color= "orange";
+        }
